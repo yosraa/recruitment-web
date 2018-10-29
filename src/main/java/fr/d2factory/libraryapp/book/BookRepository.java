3a7534ff6d502ebf502dbd219de5a64f21629b3b
yaddali <yaddali@equalios.com>
2018-10-29 21:45:54 +0100

    test commit

--- src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
+++ src/main/java/fr/d2factory/libraryapp/book/BookRepository.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.stream.Collectors.toMap;
+
 /**
  * The book repository emulates a database via 2 HashMaps
  */
@@ -14,19 +16,34 @@ public class BookRepository {
 
     public void addBooks(List<Book> books){
         //TODO implement the missing feature
+           books.stream()
+                .filter(book -> availableBooks.containsValue(book))
+                .forEach(book -> {
+                availableBooks.put(new ISBN(Long.numberOfTrailingZeros(3)),book);
+        });
+
     }
 
     public Book findBook(long isbnCode) {
         //TODO implement the missing feature
+        if (availableBooks.containsKey(isbnCode))
+            return availableBooks.get(isbnCode);
         return null;
     }
 
     public void saveBookBorrow(Book book, LocalDate borrowedAt){
         //TODO implement the missing feature
+        borrowedBooks.put(book,borrowedAt);
     }
 
     public LocalDate findBorrowedBookDate(Book book) {
         //TODO implement the missing feature
-        return null;
+        Map<Book, LocalDate> sortedByValue = borrowedBooks.entrySet()
+                     .stream()
+                     .filter( b -> b.getValue().equals( book))
+                     .collect(toMap(Map.Entry::getKey,
+                             Map.Entry::getValue));
+         return sortedByValue.get(book);
+
     }
 }
